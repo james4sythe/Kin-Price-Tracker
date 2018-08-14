80cbb63a25e56cc64455f8ec2d39b70d127141ff
James Forsythe <james.forsythe@icloud.com>
2018-08-14 16:57:06 -0400

    Initial Commit

---
+++ Kin Price Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kin Price Tracker
+//
+//  Created by James Forsythe on 8/14/18.
+//  Copyright © 2018 James Forsythe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
